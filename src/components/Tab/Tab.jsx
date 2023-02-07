60963000d66b8f560e31eb8e0eba15db22347ea2
quangdung <dungho@tizotech.com>
2023-02-07 13:21:19 +0700

    check active link

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 function Tab() {
   const nameTab =['Home','Services','News','Blog','Contact'];
- 
+  var pathArray = window.location.pathname.split('/');
+  console.log(pathArray[1]);
+
   return (
     <>
     <div className='container-tab'>
       {nameTab?.map(name=>(
-          <div className='text-name'><a href={name.toLowerCase()}>{name}</a></div>
+          <div className='text-name'><a href={name.toLowerCase()} style={{color:pathArray[1]===name.toLowerCase() ? 'white':'#AAAAAA'}}>{name}</a></div>
       )
       )}
     </div>
